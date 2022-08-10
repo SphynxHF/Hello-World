40a6598b79b7e39fd1f7846304a8f0ca2399e48f
Mike <78143189+SphynxHF@users.noreply.github.com>
2022-08-10 02:30:58 -0400

    initial commit

---
+++ Helloworld/Helloworld/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Defines the wait for usewr input then saves it as the string "name"
+            string name = Console.ReadLine();
+
+            // Says hello world and hello "user input"
+            Console.WriteLine("Hello, World And hello {0}", name);
+
+            // allows user the option to terminate by pressing enter
+            Console.WriteLine("Press Enter to teminate");
+            // Pauses the program
+            Console.Read();
+        }
+    }
+}
